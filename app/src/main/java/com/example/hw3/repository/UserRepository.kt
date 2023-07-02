866326107bc130faf2f600dfa8028443afc9290c
Тимур <tupaev05@mail.ru>
2023-07-02 21:31:37 +0300

    /

--- app/src/main/java/com/example/hw3/repository/UserRepository.kt
+++ app/src/main/java/com/example/hw3/repository/UserRepository.kt
@@ -11,7 +11,7 @@ class UserRepository {
         User(1, "https://example.com/user1", "John", "Description 1", R.drawable.user1),
         User(2, "https://example.com/user2", "Emma", "Description 2", R.drawable.user2),
         User(3, "https://example.com/user3", "Michael", "Description 3", R.drawable.user3),
-        // Добавьте остальных пользователей в список
+
     )
 
     fun getUserList(): List<User> {
